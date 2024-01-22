c351a62be92642fe3db7f7c522661f753c4cc885
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2024-01-22 12:04:49 -0300

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 function Header() {
   return (
-    <>
-      <h1 className="text-indigo-600 mt-2 uppercase">Desde Header</h1>;
-    </>
+    <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+      Patient's Follow Up  {""}
+      <span className="text-indigo-700">Veterinary</span>
+    </h1>
   );
 }
 
